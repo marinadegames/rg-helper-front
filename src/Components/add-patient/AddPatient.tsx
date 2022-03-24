f0442db63f63efede42ab5efc549610e102b9fe6
Marinade <marinadegames@gmail.com>
2022-03-24 23:48:25 +0300

    added input menu

--- src/Components/add-patient/AddPatient.tsx
+++ src/Components/add-patient/AddPatient.tsx
@@ -1,11 +1,16 @@
 import s from './AddPatient.module.css'
 import {useSelector} from "react-redux";
 import {rootReducerType} from "../../Redux/state";
+import {Button} from "../universal components/Button";
+import {useState} from "react";
+import {InputMenuTypes} from "../InputMenuResearchType/InputMenuTypes";
 
 
 export const AddPatient = () => {
 
     const nextId = useSelector<rootReducerType, number>(state => state.patients.length)
+
+    const [research, setResearch] = useState<Array<any>>([])
 
     return (
         <div className={s.add_patients}>
@@ -29,18 +34,72 @@ export const AddPatient = () => {
                 <input className={s.input_add_pat} placeholder={'ГОД'}/>
             </div>
 
-        {/*    SEX  */}
+            {/* SEX */}
             <div className={s.add_patient_element}>
                 <div className={s.name_field}>Пол:</div>
-                М
+                <div className={'mr-3'}>М</div>
                 <input className={s.input_add_pat_radio}
                        type={'radio'}
                        name={'sex'}/>
-                Ж
+                <div className={'mr-3'}>Ж</div>
                 <input className={s.input_add_pat_radio}
                        type={'radio'}
                        name={'sex'}/>
             </div>
+
+            {/* ADRESS */}
+            <div className={s.add_patient_element}>
+                <div className={s.name_field}>Адрес:</div>
+                <input className={s.input_add_pat} placeholder={'Адрес'}/>
+            </div>
+
+            {/* Тип исследования: */}
+            <div className={s.box}>
+                <div className={'flex flex-row'}>
+                    <div className={s.name_field}>Тип исследования:</div>
+                    <Button title={'Добавить'}/>
+                </div>
+
+                {/*TABLE*/}
+                <div className={'flex flex-col my-4 '}>
+                    <div className="table w-full ">
+                        <div className="table-header-group ...">
+                            <div className="table-row">
+                                <div
+                                    className="table-cell w-1/6 font-bold p-2 text-center border border-gray-400 text-left">Тип
+                                </div>
+                                <div
+                                    className="table-cell w-1/6 font-bold p-2 text-center border border-gray-400 text-left">Пленка
+                                </div>
+                                <div
+                                    className="table-cell w-1/6 font-bold p-2 text-center border border-gray-400 text-left">Кол-во
+                                </div>
+                                <div
+                                    className="table-cell w-1/6 font-bold p-2 text-center border border-gray-400 text-left">Проекций
+                                </div>
+                                <div
+                                    className="table-cell w-1/6 font-bold p-2 text-center border border-gray-400 text-left">Доза,
+                                    мЗв
+                                </div>
+                            </div>
+                        </div>
+                        <div className="table-row-group">
+                            <div className="table-row">
+                                <div className="table-cell p-2 text-start border border-gray-400">
+                                    <InputMenuTypes />
+                                </div>
+                                <div className="table-cell p-2 text-center border border-gray-400">35х35</div>
+                                <div className="table-cell p-2 text-center border border-gray-400">1</div>
+                                <div className="table-cell p-2 text-center border border-gray-400">1</div>
+                                <div className="table-cell p-2 text-center border border-gray-400">0.18</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </div>
+
 
         </div>
     )
