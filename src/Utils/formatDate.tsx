a0c78510371636ad7933cc1fa5a075d5e063fdee
marinadedev <marinadegames@gmail.com>
2022-05-23 20:19:15 +0300

    💻`v0.1.2` - add `backend`

--- src/Utils/formatDate.tsx
+++
@@ -1,8 +0,0 @@
-export const formatDate = (date: Date) => {
-    let day = date.getDay().toString()
-    let month = date.getMonth().toString()
-    let year = date.getFullYear()
-    if (date.getDay() < 10) day = '0' + date.getDay()
-    if (date.getMonth() < 10) month = '0' + date.getMonth()
-    return `${day}.${month}.${year}`
-}
