6fa3dffa368c97de47bf777ef6cda32f7eeeba24
marinadedev <marinadegames@gmail.com>
2022-05-23 22:32:08 +0300

    create nodejs server

--- README.md
+++ README.md
@@ -11,8 +11,8 @@
 🎎`v0.0.4` - added button `delete` patient => working!  |  + `layout`  
 🧢`v0.0.5` - added panel for `add patients`  
 💁`v0.0.6` - added the ability `add patients` | fix `checkbox-covid`  
-📥`v0.0.7` - added `Tailwind UI` | `mega refactoring`
-✅`v0.1.0` - `reducers` | input change | new component | add patient
+📥`v0.0.7` - added `Tailwind UI` | `mega refactoring`  
+✅`v0.1.0` - `reducers` | input change | new component | add patient  
 🔍`v0.1.1` - add `search`
 
 ## 🌠 Per aspera ad astra ⭐ 
---
+++ backend/index.js
@@ -0,0 +1,21 @@
+import http from "http";
+import fs from "fs";
+import path from "path";
+
+const server = http.createServer((req, res) => {
+
+    if (req.url === '/') {
+        fs.readFile(path.join(__dirname, 'public', 'index.html'),
+            (err, data) => {
+                if (err) throw err
+                res.writeHead(200, {
+                    'Content-Type': 'text/html'
+                })
+                res.end(data)
+            })
+    }
+})
+
+server.listen(8000, () => {
+    console.log('Server STARTED')
+})
