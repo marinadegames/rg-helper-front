2a5503df4e09877a41672e4411cc1e0a9ecfc393
marinadedev <marinadegames@gmail.com>
2022-05-24 22:09:22 +0300

    get request - users

--- backend/index.js
+++ backend/index.js
@@ -4,17 +4,34 @@ import chalk from "chalk";
 
 let server = http.createServer((req, res) => {
 
-    switch (req.url){
+    // Set CORS headers
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Request-Method', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    if (req.method === 'OPTIONS') {
+        res.writeHead(200);
+        res.end();
+        return;
+    }
+
+    switch (req.url) {
         case "/":
             res.write('HOME PAGE')
+            break
+        case '/users':
+            res.write(`[{"id": 1, "name": "Eugene"}, {"id": 2, "name": "Elina"}]`)
             break
         case '/tasks':
             res.write('TASKS PAGE')
             break
-        default: res.write('404 - PAGE NOT FOUND')
+        case '/lessons':
+            res.write('LESSONS')
+            break
+        default:
+            res.write('404 - PAGE NOT FOUND')
     }
 
-    // res.write(`<h1>HELLO nodejs!</h1>`)
     res.end()
 })
 
--- frontend/src/Components/settings/Settings.tsx
+++ frontend/src/Components/settings/Settings.tsx
@@ -1,13 +1,35 @@
 import s from "../all pateints/AllPatients.module.css";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
+type UserType = {
+    id: string,
+    name: string
+}
 
 export const Settings = () => {
+
+    const [users, setUsers] = useState<UserType[]>([])
+
+    useEffect(() => {
+        axios.get('http://localhost:7500/users')
+            .then(res => {
+                console.log(res)
+                setUsers(res.data)
+            })
+            .catch(() => {
+                console.error('ERROR!')
+            })
+    }, [])
+
     return (
         <div className={s.all_patients}>
             <div className={s.header_patient_list}>
-                Список пациентов
+                Настройки
             </div>
-
+            {users.map((u) => {
+                return <div>{u.name}</div>
+            })}
 
         </div>
     )
